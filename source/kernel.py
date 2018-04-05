a897019c731449e63dec14adeab223704d1286c0
Dalton <dvarney@thedaltonv.com>
2018-04-05 02:33:58 -0700

    Training without PCA Added

--- source/kernel.py
+++ source/kernel.py
@@ -226,12 +226,15 @@ def main():
     X, y = util.get_data('../data/subsample_data.csv')
     print "Shapes are: ", X.shape, y.shape
 
-    X_small = applyPCA(X, 500)
+
+    # We apply PCA and then train a dummy classifier
+    X_small = applyPCA(X, 684)
     metric_list = ["accuracy", "f1_score", "precision", "sensitivity", "specificity"]
     #Using PCA:
     X_trainS, X_testS, y_trainS, y_testS = train_test_split(X_small, y, test_size=0.2)
     baseline = DummyClassifier(strategy='most_frequent')
     baseline.fit(X_trainS, y_trainS)
+
     print "Baseline Metrics: ", metrics.accuracy_score(baseline.predict(X_testS), y_testS)
     # linear_svm = SVC(kernel='linear', C=1.0)
     # linear_svm.fit(X_trainS, y_trainS)
@@ -245,18 +248,28 @@ def main():
     #     print "Baseline Performance: ", performance(y_testS, baseline.predict(X_testS), metric=metric_list[metricNDX])
     #     print "SVM Performance is: ", performance(y_testS, linear_svm.predict(X_testS),metric=metric_list[metricNDX])
 
+
+    #Make our splits
     skf = StratifiedKFold(n_splits=5)
 
-    #Find optimal hyperparameters
+    # Starting RBF Params: Find optimal hyperparameters
     scoreCGvalue = {}
+    # Loop through metrics to find optimal C and gamma values for each specific metric
     for metric in metric_list:
         scoreCGvalue[metric] = list(select_param_rbf(X_trainS, y_trainS, skf, metric=metric))
     print "C and Gamma values for PCA: ", scoreCGvalue
+
+    #Lets go through the metrics again? This is efficient.
     for metricNDX in range(len(metric_list)):
+
         C, gamma = scoreCGvalue[metric_list[metricNDX]]
         print "Training with C: ", C, "and gamma: ", gamma
+
+        #Train a model with its optimal c and gamma values (Currently only RBF)
         svmRBF = SVC(kernel='rbf', C=C, gamma=gamma)
         svmRBF.fit(X_trainS, y_trainS)
+
+        #Let's see how we did!
         print "METRIC IS: ", metric_list[metricNDX]
         print "Baseline Performance: ", performance(y_testS, baseline.predict(X_testS), metric=metric_list[metricNDX])
         print "SVM Performance is: ", performance(y_testS, svmRBF.predict(X_testS), metric=metric_list[metricNDX])
