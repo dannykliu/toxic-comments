b13106dbfaae75b2856872181e2891b7840fc182
Dalton <dvarney@thedaltonv.com>
2018-04-15 15:17:14 -0700

    Added class weight functionality

--- source/phase2.py
+++ source/phase2.py
@@ -9,17 +9,14 @@ from kernel import *
 
 def main():
     #Get our data
-    X, y = util.get_data('../data/subsample_data.csv')
+    X, y = util.get_data('../data/subset_data.csv')
     print "Shapes are: ", X.shape, y.shape
 
-
-    # We apply PCA and then train a dummy classifier
-    X_small = applyPCA(X, 684)
     metric_list = ["accuracy", "f1_score", "precision", "sensitivity", "specificity"]
     #Using PCA:
-    X_trainS, X_testS, y_trainS, y_testS = train_test_split(X_small, y, test_size=0.2)
-    baseline = DummyClassifier(strategy='most_frequent')
-    baseline.fit(X_trainS, y_trainS)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    baseline = DummyClassifier(strategy='uniform')
+    baseline.fit(X_train, y_train)
 
     print "Baseline Metrics: ", metrics.accuracy_score(baseline.predict(X_testS), y_testS)
     # linear_svm = SVC(kernel='linear', C=1.0)
@@ -36,14 +33,15 @@ def main():
 
 
     #Make our splits
-    skf = StratifiedKFold(n_splits=5)
 
     # Starting RBF Params: Find optimal hyperparameters
     scoreCGvalue = {}
     # Loop through metrics to find optimal C and gamma values for each specific metric
     for metric in metric_list:
-        scoreCGvalue[metric] = list(select_param_rbf(X_trainS, y_trainS, skf, metric=metric))
-    print "C and Gamma values for PCA: ", scoreCGvalue
+        skf = StratifiedKFold(n_splits=5)
+        scoreCGvalue[metric] = \
+        list(select_param_rbf(X_train, y_train, skf metric=metric, class_weight='balanced'))
+    print "C and Gamma values Training: ", scoreCGvalue
 
     #Lets go through the metrics again? This is efficient.
     for metricNDX in range(len(metric_list)):
@@ -52,13 +50,13 @@ def main():
         print "Training with C: ", C, "and gamma: ", gamma
 
         #Train a model with its optimal c and gamma values (Currently only RBF)
-        svmRBF = SVC(kernel='rbf', C=C, gamma=gamma)
-        svmRBF.fit(X_trainS, y_trainS)
+        svmRBF = SVC(kernel='rbf', C=C, gamma=gamma, class_weight= 'balanced')
+        svmRBF.fit(X_train, y_train)
 
         #Let's see how we did!
         print "METRIC IS: ", metric_list[metricNDX]
-        print "Baseline Performance: ", performance(y_testS, baseline.predict(X_testS), metric=metric_list[metricNDX])
-        print "SVM Performance is: ", performance(y_testS, svmRBF.predict(X_testS), metric=metric_list[metricNDX])
+        print "Baseline Performance: ", performance(y_test, baseline.predict(X_test), metric=metric_list[metricNDX])
+        print "SVM Performance is: ", performance(y_test, svmRBF.predict(X_test), metric=metric_list[metricNDX])
 
 
 
