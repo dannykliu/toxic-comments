93fbd1e12d6f1575e566d18c8d4a8846c2bdd8ff
Danny Liu <dannyliu@WL-217-34.WPA.Claremont.Edu>
2018-04-15 19:58:46 -0700

    tuned rbf

--- source/driver.py
+++ source/driver.py
@@ -36,7 +36,7 @@ def main():
     # C_range = 10.0 ** np.arange(-3, 3)
     C_range = [0.1]
     for i in range(len(C_range)):
-        beta = 1.5
+        beta = 2
         t1 = time.time()
         # weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
         # cw = {0: weights[0], 1: weights[1]}
@@ -56,6 +56,33 @@ def main():
         print("linear recall ", recall)
         print("linear svm f1 score", f1)
         print("linear svm fbeta score", fbeta)
+
+    # C_range = np.logspace(-2, 3, 6)
+    # gamma_range = np.logspace(-4, 1, 6)
+    # C_range = [0.1]
+    # gamma_range = [10]
+    # for i in range(len(C_range)):
+    #     for j in range(len(gamma_range)):
+    #         beta = 2
+    #         t1 = time.time()
+    #         # weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
+    #         # cw = {0: weights[0], 1: weights[1]}
+    #         linear_svm = SVC(kernel='linear', C=C_range[i], gamma=gamma_range[j], class_weight='balanced')
+    #         linear_svm.fit(X_train, y_train)
+    #
+    #         print('-------------------------')
+    #         print("C: ", C_range[i], "gamma: ", gamma_range[j])
+    #         print('training took ' + str(time.time() - t1) + ' seconds')
+    #         y_pred = linear_svm.predict(X_test)
+    #         precision = metrics.precision_score(y_true=y_test, y_pred=y_pred)
+    #         recall = metrics.recall_score(y_true=y_test, y_pred=y_pred)
+    #         f1 = precision * recall / (precision + recall)
+    #         fbeta = (1 + beta ** 2) * precision * recall / (beta ** 2 * precision + recall)
+    #         print("rbf svm accuracy ", metrics.accuracy_score(y_true=y_test, y_pred=y_pred))
+    #         print("rbf svm precision ", precision)
+    #         print("rbf recall ", recall)
+    #         print("rbf svm f1 score", f1)
+    #         print("rbf svm fbeta score", fbeta)
 
 
 if __name__ == '__main__':
