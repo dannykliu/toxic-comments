d06c5a7c1dac27ee5a23f389de4e10fcb16abf3a
Dalton <dvarney@thedaltonv.com>
2018-04-15 17:49:57 -0700

    Cleaned up phase2

--- source/phase2.py
+++ source/phase2.py
@@ -16,28 +16,22 @@ def train(inputList):
 
 def main():
     #Get our data
+    #USE GETDATA2 NOW
     X, y = util.get_data('../data/subset.csv')
     print ("Shapes are: ", X.shape, y.shape)
 
+    # vect = TfidfVectorizer(max_features=2000, min_df=2)
+    # X_dtm = vect.fit_transform(X)
+    print(X_dtm.shape, y.shape)
+
     metric_list = ["accuracy", "f1_score", "precision", "sensitivity", "specificity"]
-    #Using PCA:
+    # still need to add in tf-idf implementation
+    #X_train, X_test, y_train, y_test = train_test_split(X_dtm, y, test_size=0.2)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     baseline = DummyClassifier(strategy='uniform')
     baseline.fit(X_train, y_train)
 
     print ("Baseline Metrics: ", metrics.accuracy_score(baseline.predict(X_test), y_test))
-    # linear_svm = SVC(kernel='linear', C=1.0)
-    # linear_svm.fit(X_trainS, y_trainS)
-    #
-    # for metricNDX in range(len(metric_list)):
-    #     print "METRIC IS: ", metric_list[metricNDX]
-    #     if metric_list[metricNDX] == "auroc":
-    #         print "Omitting Baseline due to metric"
-    #         print "Omitting SVM due to metric"
-    #         #print "SVM Performance is: ", performance(y_testS, linear_svm.decision_function(X_testS), metric=metric_list[metricNDX])
-    #     print "Baseline Performance: ", performance(y_testS, baseline.predict(X_testS), metric=metric_list[metricNDX])
-    #     print "SVM Performance is: ", performance(y_testS, linear_svm.predict(X_testS),metric=metric_list[metricNDX])
-
 
     #Make our splits
     inputs = []
@@ -69,32 +63,6 @@ def main():
         print ("METRIC IS: ", metric_list[metricNDX])
         print ("Baseline Performance: ", performance(y_test, baseline.predict(X_test), metric=metric_list[metricNDX]))
         print ("SVM Performance is: ", performance(y_test, svmRBF.predict(X_test), metric=metric_list[metricNDX]))
-
-
-
-
-    ### Regular testing (without PCA)
-    #Baseline training
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    # baseline = DummyClassifier(strategy='most_frequent')
-    # baseline.fit(X_train, y_train)
-    # print "Baseline Metrics: ", metrics.accuracy_score(baseline.predict(X_test), y_test)
-    #
-    # skf = StratifiedKFold(n_splits=5)
-    # metric_list = ["accuracy", "f1_score", "auroc", "precision", "sensitivity", "specificity"]
-    # #Find optimal hyperparameters
-    # scoreCGvalue = {}
-    # for metric in metric_list:
-    #     scoreCGvalue[metric] = list(select_param_rbf(X_train, y_train, skf, metric=metric))
-    # print "C and Gamma Values: ", scoreCGvalue
-
-
-    #SVM training
-    # svmRBF = SVC(kernel='rbf', C=1.0)
-    # linear_svm.fit(X_train, y_train)
-    # print 'done training'
-    # print metrics.accuracy_score(linear_svm.predict(X_test), y_test)
-    # print np.equal(linear_svm.predict(X_test), baseline.predict(X_test))
 
 
 if __name__ == '__main__':
