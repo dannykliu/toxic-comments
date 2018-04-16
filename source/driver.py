40233b6a5d4af797e6d16b77efac5390e7dce49e
Danny Liu <dannyliu@WL-217-34.WPA.Claremont.Edu>
2018-04-15 17:42:12 -0700

    c=0.1 works well

--- source/driver.py
+++ source/driver.py
@@ -33,18 +33,29 @@ def main():
     print("baseline precision ", metrics.precision_score(y_true=y_test, y_pred=y_pred))
     print("baseline recall ", metrics.recall_score(y_true=y_test, y_pred=y_pred))
 
-    t1 = time.time()
-    # weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
-    # cw = {0: weights[0], 1: weights[1]}
-    linear_svm = SVC(kernel='linear', C=1.0, class_weight='balanced')
-    linear_svm.fit(X_train, y_train)
-    print('-------------------------')
-    print('training took ' + str(time.time() - t1) + ' seconds')
+    # C_range = 10.0 ** np.arange(-3, 3)
+    C_range = [0.1]
+    for i in range(len(C_range)):
+        beta = 1.5
+        t1 = time.time()
+        # weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
+        # cw = {0: weights[0], 1: weights[1]}
+        linear_svm = SVC(kernel='linear', C=C_range[i], class_weight='balanced')
+        linear_svm.fit(X_train, y_train)
 
-    y_pred = linear_svm.predict(X_test)
-    print("linear svm accuracy ", metrics.accuracy_score(y_true=y_test, y_pred=y_pred))
-    print("linear svm precision ", metrics.precision_score(y_true=y_test, y_pred=y_pred))
-    print("linear recall ", metrics.recall_score(y_true=y_test, y_pred=y_pred))
+        print('-------------------------')
+        print("C: ", C_range[i])
+        print('training took ' + str(time.time() - t1) + ' seconds')
+        y_pred = linear_svm.predict(X_test)
+        precision = metrics.precision_score(y_true=y_test, y_pred=y_pred)
+        recall = metrics.recall_score(y_true=y_test, y_pred=y_pred)
+        f1 = precision * recall / (precision + recall)
+        fbeta = (1+beta**2)*precision*recall/(beta**2 * precision + recall)
+        print("linear svm accuracy ", metrics.accuracy_score(y_true=y_test, y_pred=y_pred))
+        print("linear svm precision ", precision)
+        print("linear recall ", recall)
+        print("linear svm f1 score", f1)
+        print("linear svm fbeta score", fbeta)
 
 
 if __name__ == '__main__':
